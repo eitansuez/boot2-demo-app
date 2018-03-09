7223e9dc9d27c9e35fda0c7917a2a7681a60cf70
Eitan Suez <esuez@pivotal.io>
2018-03-08 22:20:38 -0600

    added actuator dependency, configuring management endpoints, custom health indicator

---
+++ src/main/java/com/example/demo/MemoryHealthIndicator.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemoryHealthIndicator implements HealthIndicator {
+
+  @Override
+  public Health health() {
+    Runtime runtime = Runtime.getRuntime();
+    long freeMemory = runtime.freeMemory();
+    long totalMemory = runtime.totalMemory();
+    double percentFree = freeMemory / (double) totalMemory;
+
+    Health.Builder builder = (percentFree < 0.1) ? Health.down() : Health.up();
+    return builder.withDetail("total", totalMemory)
+        .withDetail("free", freeMemory)
+        .withDetail("percentFree", percentFree)
+        .build();
+  }
+}
